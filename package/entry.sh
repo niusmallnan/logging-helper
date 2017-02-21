48ce53b11db39bd5154351effd42cebc0a88a9df
niusmallnan <zhangzhibo521@gmail.com>
2017-02-21 18:27:00 +0800

    remove /var/run

--- package/entry.sh
+++ package/entry.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for i in $(curl -s --unix /var/run/docker.sock http://localhost/info | jq -r .DockerRootDir) /var/lib/docker /run /var/run; do
+for i in $(curl -s --unix /var/run/docker.sock http://localhost/info | jq -r .DockerRootDir) /var/lib/docker /run; do
     for m in $(tac /proc/mounts | awk '{print $2}' | grep ^${i}/); do
         umount $m || true
     done
